0a7a7b11c55b2bad1b7885b263d607256f9ad1e1
yinyang <1134555105@qq.com>
2019-07-18 11:00:40 +0800

    断路器

--- scloud-client/src/main/java/com/example/scloudclient/ScloudClientApplication.java
+++ scloud-client/src/main/java/com/example/scloudclient/ScloudClientApplication.java
@@ -1,9 +1,17 @@
 package com.example.scloudclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 // 注册到eureka
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+//开启断路器
+@EnableHystrix
+//开启HystrixDashboard
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ScloudClientApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(ScloudClientApplication.class, args);
@@ -20,9 +35,24 @@ public class ScloudClientApplication {
     @Value("${server.port}")
     String port;
 
+    @HystrixCommand(fallbackMethod = "hi")
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    @HystrixCommand(fallbackMethod = "hi")
+    public String hi(String name) {
+        return "hi " + name + " , server is down";
+    }
+
+    @Bean(name = "hystrixRegistrationBean")
+    public ServletRegistrationBean servletRegistrationBean() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(
+                new HystrixMetricsStreamServlet(), "/hystrix.stream");
+        registration.setName("hystrixServlet");
+        registration.setLoadOnStartup(1);
+        return registration;
     }
 
 }
